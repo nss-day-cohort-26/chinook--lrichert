4095a377d73d311185bf9098b9b0875c79deb571
Lauren Richert <lsrichert@gmail.com>
2018-09-24 08:35:52 -0500

    modified total_invoices sql to count invoices

--- total_invoices_{year}.sql
+++ total_invoices_{year}.sql
@@ -1,4 +1,6 @@
 --How many Invoices were there in 2009 and 2011?
-Select i.InvoiceId, i.InvoiceDate
+Select COUNT (i.InvoiceId) AS Invoices, STRFTIME('%Y', i.InvoiceDate) AS YEAR
 FROM Invoice i
-WHERE InvoiceDate LIKE '%2009%' OR InvoiceDate LIKE '%2011%';
+WHERE InvoiceDate LIKE '%2009%' OR InvoiceDate LIKE '%2011%'
+GROUP BY Year;
+
